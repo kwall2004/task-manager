2d58761e621d6d803f668bceb9a8e1d2b4bb4592
kwall2004 <kwall2004@gmail.com>
2016-05-13 13:24:23 -0400

    add MVC project

---
+++ TaskManager.MVC/Models/TaskViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManager.MVC.Models
+{
+    public class TaskViewModel
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ TaskManager.MVC/Models/TasksViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TaskManager.MVC.Models
+{
+    public class TasksViewModel
+    {
+        public List<TaskModel> Tasks { get; set; }
+        public int ActiveCount { get; set; }
+    }
+}
